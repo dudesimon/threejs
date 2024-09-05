1afa5f4e97fed2c7e3484ba93594d90c3466a756
Simon Sierra <sdanielsierra@southern.edu>
2024-09-05 00:29:05 -0400

    added moon, added ring, added stars

--- TrialZero/src/App.tsx
+++ TrialZero/src/App.tsx
@@ -1,6 +1,7 @@
 // src/App.tsx
 import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const App: React.FC = () => {
   const mountRef = useRef<HTMLDivElement>(null);
@@ -16,26 +17,74 @@ const App: React.FC = () => {
       console.error('Canvas element with ID "background" not found.');
       return;
     }
+    //Camera
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({ canvas });
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.z = 30
 
+    //Objects
     const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-    const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });
+    const material = new THREE.MeshStandardMaterial({ color: 0xFF6347});
     const torus = new THREE.Mesh(geometry, material);
 
     scene.add(torus);
 
-    camera.position.z = 30
+    function addStar(): void {
+      const geometry = new THREE.SphereGeometry(0.25, 32, 32);
+      const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+      const star = new THREE.Mesh(geometry, material);
+      const [x, y, z] = Array(3).fill(0).map(() => THREE.MathUtils.randFloatSpread( 100 ))
+      star.position.set(x, y, z);
+      scene.add(star);
+    }
+    // Loop to add stars
+    for (let i = 0; i < 200; i++)
+      addStar()
+
+    const moonTexture = new THREE.TextureLoader().load('./src/assets/moon.jpg')
+    const normalTexture = new THREE.TextureLoader().load('./src/assets/normal.jpg')
+    const moon = new THREE.Mesh(
+      new THREE.SphereGeometry(3, 60, 60),
+      new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: normalTexture })
+    )
+    scene.add(moon)
+    
+
+    //Lighting
+    const pointLight = new THREE.PointLight(0xffffff)
+    pointLight.position.set(5, 5, 5);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff)
+    scene.add(pointLight, ambientLight);
+
+    //Helper
+    // const lightHelper = new THREE.PointLightHelper(pointLight)
+    // scene.add(lightHelper)
+    const gridHelper = new THREE.GridHelper(200, 50)
+    scene.add(gridHelper)
+
+    //Controller
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    //Texture Packs
+    //can add a callback for when we want a loading bar for alot of static images
+    scene.background = new THREE.Color(0x00051F)
+    
+    
+
+
+    
 
     // Animation loop
     const animate = () => {
       requestAnimationFrame(animate);
       torus.rotation.x += 0.01
-      torus.rotation.y += 0.01
+      torus.rotation.y += 0.005
       renderer.render(scene, camera);
+      controls.update();
     };
     animate();
 
